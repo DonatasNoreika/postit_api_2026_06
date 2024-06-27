4c8808ac64c7c8390064325f38143b6dae53866e
DonatasNoreika <donoras@gmail.com>
2024-06-27 09:56:17 +0300

    Sukurtas PostList API viewas

---
+++ api_example/postit_api/serializers.py
@@ -0,0 +1,9 @@
+
+from rest_framework import serializers
+from .models import Post, Comment, PostLike, CommentLike
+
+class PostSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Post
+        fields = ['id', 'user', 'title', 'body', 'created']
+
