c3b7eb8e84dda27385d40d28b76088c5e2bfc946
DonatasNoreika <donoras@gmail.com>
2024-06-27 11:27:32 +0300

    Savininkui galima trinti/redaguoti komentarus. Kitiems rodoma klaida.

--- api_example/postit_api/urls.py
+++ api_example/postit_api/urls.py
@@ -1,10 +1,11 @@
 from django.contrib import admin
 from django.urls import path, include
-from .views import PostList, PostDetail, CommentList
+from .views import PostList, PostDetail, CommentList, CommentDetail
 
 urlpatterns = [
     path('api-auth/', include('rest_framework.urls', namespace='rest_framework')),
     path("posts/", PostList.as_view()),
     path("posts/<int:pk>", PostDetail.as_view()),
     path("posts/<int:pk>/comments", CommentList.as_view()),
+    path("comments/<int:pk>", CommentDetail.as_view()),
 ]
