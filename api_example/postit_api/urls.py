f7f2f6b016f4aa719b3b89da66819652e1a0fcc4
DonatasNoreika <donoras@gmail.com>
2024-06-27 10:52:33 +0300

    Prisijungęs vartotojas gali trinti ir redaguoti tik savo įrašus

--- api_example/postit_api/urls.py
+++ api_example/postit_api/urls.py
@@ -1,7 +1,9 @@
 from django.contrib import admin
-from django.urls import path
-from .views import PostList
+from django.urls import path, include
+from .views import PostList, PostDetail
 
 urlpatterns = [
     path("posts/", PostList.as_view()),
+    path("posts/<int:pk>", PostDetail.as_view()),
+    path('api-auth/', include('rest_framework.urls', namespace='rest_framework'))
 ]
--- api_example/postit_api/views.py
+++ api_example/postit_api/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from rest_framework import generics, permissions
 from .models import Post, Comment, PostLike, CommentLike
 from .serializers import PostSerializer
+from rest_framework.exceptions import ValidationError
 
 # Create your views here.
 
@@ -12,4 +13,25 @@ class PostList(generics.ListCreateAPIView):
 
     def perform_create(self, serializer):
         serializer.save(user=self.request.user)
+
+
+class PostDetail(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Post.objects.all()
+    serializer_class = PostSerializer
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
+
+    def delete(self, request, *args, **kwargs):
+        post = Post.objects.filter(pk=kwargs['pk'], user=self.request.user)
+        if post.exists():
+            return super().delete(request, *args, **kwargs)
+        else:
+            raise ValidationError("Negalima trinti svetimų pranešimų!")
+
+    def put(self, request, *args, **kwargs):
+        post = Post.objects.filter(pk=kwargs['pk'], user=self.request.user)
+        if post.exists():
+            return super().put(request, *args, **kwargs)
+        else:
+            raise ValidationError("Negalima redaguoti svetimų pranešimų!")
+
 
